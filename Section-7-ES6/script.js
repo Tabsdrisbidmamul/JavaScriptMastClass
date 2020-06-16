12d8172a9c704ac1e78102950524c2aa714831c9
Idris Khan <idris.khan303@gmail.com>
2020-06-16 11:38:33 +0100

    Changes made to script and now using VS Code Editor

--- Section-7-ES6/script.js
+++ Section-7-ES6/script.js
@@ -469,16 +469,16 @@ Like so
     SmithPerson("John, undefined, 1990);
     
 */
-function SmithPerson(firstName, yearOfBirth, lastName="Smith", nationality="American") {
+/* function SmithPerson(firstName, yearOfBirth, lastName="Smith", nationality="American") {
     this.firstName = firstName;
     this.yearOfBirth = yearOfBirth;
     this.lastName = lastName;
     this.nationality = nationality;
 
-}
+} */
 
-var john = new SmithPerson("John", 1990);
-var emily = new SmithPerson("Emily", 1983, "Diaz", "Spanish");
+// var john = new SmithPerson("John", 1990);
+// var emily = new SmithPerson("Emily", 1983, "Diaz", "Spanish");
 
 
 /* ------------------------------------------
