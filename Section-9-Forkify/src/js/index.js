6a83d093697309530f39b2c7aa6b252e6247c9c2
Idris Khan <idris.khan303@gmail.com>
2020-06-19 19:16:21 +0100

    Search Model implemented now going to searchView

--- Section-9-Forkify/src/js/index.js
+++ Section-9-Forkify/src/js/index.js
@@ -6,3 +6,8 @@
 // api get: https://forkify-api.herokuapp.com/api/get
 // api get example: https://forkify-api.herokuapp.com/api/get?rId=47746
 
+import Search from "./models/Search";
+
+const search = new Search("pizza");
+search.getResults();
+console.log(search);
