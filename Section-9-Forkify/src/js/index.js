f820df4a041b0ded753a49fa191db1e68e1e5db5
Idris Khan <idris.khan303@gmail.com>
2020-06-19 17:55:18 +0100

    Babel and Webpack setup completed, moving onto AJAX

--- Section-9-Forkify/src/js/index.js
+++ Section-9-Forkify/src/js/index.js
@@ -1,1 +1,8 @@
 // Global app controller
+
+// api: https://forkify-api.herokuapp.com/api/search
+// api search example:  https://forkify-api.herokuapp.com/api/search?q=pizza
+
+// api get: https://forkify-api.herokuapp.com/api/get
+// api get example: https://forkify-api.herokuapp.com/api/get?rId=47746
+
