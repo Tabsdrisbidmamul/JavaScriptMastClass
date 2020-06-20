21d20d8e0596503052c9713313618b6e60393e95
Idris Khan <idris.khan303@gmail.com>
2020-06-20 12:05:22 +0100

    Finished pagination logic

--- Section-9-Forkify/src/js/views/searchView.js
+++ Section-9-Forkify/src/js/views/searchView.js
@@ -8,6 +8,7 @@ export const clearInput = () => {
 
 export const clearResults = () => {
     elements.searchResultsList.innerHTML = "";
+    elements.searchResPages.innerHTML = "";
 }
 
 const renderRecipe = recipe => {
@@ -41,7 +42,47 @@ const renderRecipe = recipe => {
     elements.searchResultsList.insertAdjacentHTML("beforeend", markup);
 }; 
 
-export const renderResults = recipes => {
-    recipes.forEach(renderRecipe);
+// type: 'prev' or 'next'
+// returns markup
+const createButton = (page, type) => `
+    <button class="btn-inline results__btn--${type}" data-goto=${type === "prev" ? page - 1 : page + 1}>
+        <span>Page ${type === "prev" ? page - 1 : page + 1}</span>  
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${type === "prev" ? "left" : "right"}"></use>
+        </svg>
+    </button>
+`;
 
+
+const renderButtons = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage);
+
+    let btn;
+    if(page === 1 && pages > 1) {
+        // Only Button to go to the next page
+        btn = createButton(page, "next");
+    } else if (page < pages) {
+        // Both Buttons
+        btn = `
+        ${createButton(page, "prev")} 
+        ${createButton(page, "next")}
+        `;
+    } else if (page === pages && pages > 1) {
+        // Only Button to go to the prev page
+        btn = createButton(page, "prev");
+    }
+
+    if (btn) 
+    elements.searchResPages.insertAdjacentHTML("afterbegin", btn);
+}; 
+
+export const renderResults = (recipes, page=1, resPerPage=10) => {
+    // render results of current page
+    const start = (page - 1) * resPerPage;
+    const end = page * resPerPage;
+
+    recipes.slice(start, end).forEach(renderRecipe);
+
+    // render pagination button]
+    renderButtons(page, recipes.length, resPerPage);
 };
